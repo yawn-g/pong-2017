2cc9f7930f089fe6017eaadd64f6e2728c131d30
YQN <spam-me@yann-g.net>
2016-12-19 22:08:30 +0100

    Add files via upload

--- pong-2017.ino
+++ pong-2017.ino
@@ -127,6 +127,10 @@ extern const byte font3x3[];
 Ball ball;
 
 void drawBackground() {
+  byte yOffset;
+  if (ball.y < 8) yOffset = ball.y;
+  else yOffset = 8;
+
   gb.display.clear();
 
   // top & bottom borders
@@ -140,23 +144,23 @@ void drawBackground() {
 
   // names
   gb.display.cursorX = 29;
-  gb.display.cursorY = 2;
+  gb.display.cursorY = 2 - (8-yOffset);
   gb.display.print("YQN");
   gb.display.cursorX = 44;
   gb.display.print("RLF");
 
   // life gauges
-  gb.display.fillRect(1, 2, 27, 3);
-  gb.display.drawPixel(0, 3);
-  gb.display.fillRect(56, 2, 27, 3);
-  gb.display.drawPixel(83, 3);
+  gb.display.fillRect(1, 2 - (8 -yOffset), 27, 3);
+  gb.display.drawPixel(0, 3 - (8-yOffset));
+  gb.display.fillRect(56, 2-(8-yOffset), 27, 3);
+  gb.display.drawPixel(83, 3 - (8-yOffset));
 
   // rounds won
   for (byte p = 0; p < 2; p++) {
     for (byte i = 0; i < player[p].roundsWon; i++) {
       int dir = player[p].roundsBarDir;
       byte x = player[p].roundsBarX + i*4*dir;
-      gb.display.drawFastHLine(x, 6, 3);
+      gb.display.drawFastHLine(x, 6 -(8-yOffset), 3);
     }    
   }
 }
