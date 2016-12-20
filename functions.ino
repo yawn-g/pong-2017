e4c7a59b08df537261ea24c72510fc1ac822335a
YQN <spam-me@yann-g.net>
2016-12-20 18:27:50 +0100

    Add files via upload

---
+++ functions.ino
@@ -0,0 +1,40 @@
+void drawBackground() {
+  byte yOffset;
+  if (ball.y < 8) yOffset = ball.y;
+  else yOffset = 8;
+
+  gb.display.clear();
+
+  // top & bottom borders
+  gb.display.setColor(BLACK);
+  gb.display.drawFastHLine(0, 0, LCDWIDTH);
+  gb.display.drawFastHLine(0, 47, LCDWIDTH);
+
+  // net
+  gb.display.setColor(GRAY);
+  gb.display.fillRect(41, 1, 2, 46);
+
+  // names
+  gb.display.cursorX = 29;
+  gb.display.cursorY = 2 - (8-yOffset);
+  gb.display.print("YQN");
+  gb.display.cursorX = 44;
+  gb.display.print("RLF");
+
+  // life gauges
+  gb.display.fillRect(1, 2 - (8 -yOffset), 27, 3);
+  gb.display.drawPixel(0, 3 - (8-yOffset));
+  gb.display.fillRect(56, 2-(8-yOffset), 27, 3);
+  gb.display.drawPixel(83, 3 - (8-yOffset));
+
+  // rounds won
+  for (byte p = 0; p < 2; p++) {
+    for (byte i = 0; i < player[p].roundsWon; i++) {
+      int dir = player[p].roundsBarDir;
+      byte x = player[p].roundsBarX + i*4*dir;
+      gb.display.drawFastHLine(x, 6 -(8-yOffset), 3);
+    }    
+  }
+}
+
+
--- pong-2017.ino
+++ pong-2017.ino
@@ -76,7 +76,7 @@ class Ball {
   public:
     Ball();
     byte x, y;
-    int xSpeed, ySpeed;
+    float xSpeed, ySpeed;
     void draw();
     void move();
     void padCollide();
@@ -128,44 +128,7 @@ extern const byte font3x3[];
 
 Ball ball;
 
-void drawBackground() {
-  byte yOffset;
-  if (ball.y < 8) yOffset = ball.y;
-  else yOffset = 8;
-
-  gb.display.clear();
-
-  // top & bottom borders
-  gb.display.setColor(BLACK);
-  gb.display.drawFastHLine(0, 0, LCDWIDTH);
-  gb.display.drawFastHLine(0, 47, LCDWIDTH);
-
-  // net
-  gb.display.setColor(GRAY);
-  gb.display.fillRect(41, 1, 2, 46);
-
-  // names
-  gb.display.cursorX = 29;
-  gb.display.cursorY = 2 - (8-yOffset);
-  gb.display.print("YQN");
-  gb.display.cursorX = 44;
-  gb.display.print("RLF");
-
-  // life gauges
-  gb.display.fillRect(1, 2 - (8 -yOffset), 27, 3);
-  gb.display.drawPixel(0, 3 - (8-yOffset));
-  gb.display.fillRect(56, 2-(8-yOffset), 27, 3);
-  gb.display.drawPixel(83, 3 - (8-yOffset));
-
-  // rounds won
-  for (byte p = 0; p < 2; p++) {
-    for (byte i = 0; i < player[p].roundsWon; i++) {
-      int dir = player[p].roundsBarDir;
-      byte x = player[p].roundsBarX + i*4*dir;
-      gb.display.drawFastHLine(x, 6 -(8-yOffset), 3);
-    }    
-  }
-}
+void drawBackground();
 
 void setup() {
   Serial.begin(9600);
@@ -193,6 +156,9 @@ void loop() {
 
     if (gb.buttons.repeat(BTN_UP, 1)) player[0].moveUp();
     if (gb.buttons.repeat(BTN_DOWN, 1)) player[0].moveDown();
-    if (gb.buttons.pressed(BTN_C)) gb.titleScreen(F("PONG 2017"));
+    if (gb.buttons.pressed(BTN_C)) {
+      gb.titleScreen(F("PONG 2017"));
+      gb.battery.show = false;
+    }
   }
 }
